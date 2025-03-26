1db14949e8cd3773beefc53568665dec271530af
NdzalamaKhosa <st10438552@vgconnect.edu.za>
2025-03-26 14:11:52 +0200

    Added a bit more to environments

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -5,9 +5,10 @@ public class Generator : MonoBehaviour
 {
     public GameObject[] Sections = new GameObject[3];
 
-    public int zPos = 50;
+    public float zPos = 43.99641f;//stores the position of new spawn location
     public bool isCreating = false;
-    public int segmentNum;
+    public int sectionNum;
+    public int prevSegment = -1;//stores the index of previous spawned section
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Update()
@@ -22,10 +23,20 @@ public class Generator : MonoBehaviour
 
     IEnumerator Gen()
     {
-        segmentNum = Random.Range(0, 3);
-        Instantiate(Sections[segmentNum], new Vector3(-6.999076f, -7.195025f, zPos), Quaternion.identity);
-        zPos += 50;
-        yield return new WaitForSeconds(10);
+        sectionNum = Random.Range(0, 3);
+        
+        //ensures the same section doesn't spawn twice in a row
+        if (sectionNum == prevSegment) 
+        { 
+            while (sectionNum == prevSegment)
+                sectionNum = Random.Range(0, 3); 
+        }
+
+        Instantiate(Sections[sectionNum], new Vector3(-6.999076f, -7.195025f, zPos), Quaternion.identity);
+        zPos += 43.99641f;
+        yield return new WaitForSeconds(4);
         isCreating = false;
+
+        prevSegment = sectionNum;
     }
 }
